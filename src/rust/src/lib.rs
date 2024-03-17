203e4610780c933c23daea144f6d33750ef1f28e
RileyLeff <rileyleff@gmail.com>
2024-03-17 14:42:08 -0400

    fuck it, trying workflow in different repo

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -1,5 +1,5 @@
 use extendr_api::prelude::*;
-use polars::prelude::*;
+use plomo::{models::sperry, Model};
 
 /// Return string `"Hello world!"` to R.
 /// @export
@@ -15,31 +15,41 @@ fn verify_install() -> &'static str {
     "Package build was successful"
 }
 
-/// Do stuff with polars
+/// Passes a config to a model, then runs it. 
 /// @export
 #[extendr]
-fn do_polars_thing(cool_number: f64) -> &'static str {
-    let df = df! [
-        "names" => ["a", "b", "c"],
-        "values" => [1f64, 2f64, 3f64],
-        "values_nulls" => [Some(1), None, Some(3)]
-    ].expect("fucked up the polars thing");
+fn model_runner(
+    path_to_config: String,
+    path_to_data: String, 
+    path_to_output: String
+) -> &'static str {
+    let m = sperry::SperryModel::try_new_from_paths(path_to_config, path_to_data)
+        .expect("bad model, fail");
 
-    let vals = df.column("values").expect("broken vals");
-    let mask = vals.gt(cool_number).expect("broken mask");
-
-    let filtered_vals = vals.filter(&mask).expect("broken filter");
-
-    let fvals_sum:f64 = filtered_vals.sum().expect("broken sum");
-
-    if fvals_sum > cool_number {
-        "polars thing was successful and cool_num was tiny"
-    } else {
-        "polars thing was successful and cool_num was huge"
-    }
+    let o = m.execute(path_to_output);
     
-    
+    // return o;
+
+    let s: &'static str = "This is a static string";
+
+    return s;
 }
+
+/// saves config to a path 
+/// @export
+#[extendr]
+fn write_default_config(
+    path_to_write: String
+) -> &'static str {
+    let _x = sperry::SperryConfig::serialize_default_to_path(path_to_write);
+    //return String::from("ok")
+    let s: &'static str = "This is a static string";
+
+    return s;
+}
+
+
+
 
 // Macro to generate exports.
 // This ensures exported functions are registered with R.
@@ -48,5 +58,6 @@ extendr_module! {
     mod testbuild;
     fn hello_world;
     fn verify_install;
-    fn do_polars_thing;
+    fn model_runner;
+    fn write_default_config;
 }
