92d3cd2863f6dc5e6b7b28af698a7481cd85e500
RileyLeff <rileyleff@gmail.com>
2024-03-19 16:13:28 -0400

    yowza

---
+++ .github/util/setup_ubuntu_external_deps.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt-get install -y \
+    libharfbuzz-dev \
+    libfribidi-dev \
+    libcurl4-openssl-dev \
+    libgit2-dev \
+    libxml2-dev \
+    libfontconfig1-dev \
+    libfreetype6-dev \
+    libpng-dev \
+    libtiff5-dev \
+    libjpeg-dev
